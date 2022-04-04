9f0b5ad0a9c1c1e98595b89792ba70f23d289c17
xareni <xareni_00@hotmail.com>
2022-04-03 23:42:30 -0500

    producto responsivo y tallas dinamicas

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -12,45 +12,38 @@
                 
                 <div class="container-fluid pt-4" style="overflow-x:scroll;">
                     <div class="row flex-row flex-nowrap">
-                        <div class="col-3 d-flex justify-content-center ">
+                        <div class="col-4 col-md-5 col-lg-3 col-xl-4 col-xxl-3 d-flex justify-content-center ">
                             <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
+                                <div class="card" >
+                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                                 </div>
                             </a>
                         </div>
-                        <div class="col-3 d-flex justify-content-center">
+                        <div class="col-4 col-md-5 col-lg-3 col-xl-4 col-xxl-3 d-flex justify-content-center ">
                             <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
+                                <div class="card" >
+                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                                 </div>
                             </a>
                         </div>
-                        <div class="col-3 d-flex justify-content-center">
+                        <div class="col-4 col-md-5 col-lg-3 col-xl-4 col-xxl-3 d-flex justify-content-center ">
                             <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
+                                <div class="card" >
+                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                                 </div>
                             </a>
                         </div>
-                        <div class="col-3 d-flex justify-content-center">
+                        <div class="col-4 col-md-5 col-lg-3 col-xl-4 col-xxl-3 d-flex justify-content-center ">
                             <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
+                                <div class="card" >
+                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                                 </div>
                             </a>
                         </div>
-                        <div class="col-3 d-flex justify-content-center">
+                        <div class="col-4 col-md-5 col-lg-3 col-xl-4 col-xxl-3  d-flex justify-content-center ">
                             <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-3 d-flex justify-content-center">
-                            <a href="">
-                                <div class="card " style="width: 7rem;">
-                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0" alt="...">
+                                <div class="card" >
+                                    <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                                 </div>
                             </a>
                         </div>
@@ -63,11 +56,15 @@
                     <h1 id="nameProduct" class="fw-bold">Vestido en manta</h1>
                     <h4 id ="descriptionProduct" class="fw-bold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate pariatur excepturi unde voluptatem rem consectetur dolorem ut. Ullam officiis, ut dolore delectus perferendis asperiores! Assumenda omnis voluptatum atque nisi repudiandae.</h4>
                     <div class="d-flex justify-content-between mt-3">
-                        <button type="button" class="btn btn-outline-dark col-2 d-block p-2"> <h3 class="my-0 py-0" >XS</h3></button>
+                        <!--<button type="button" class="btn btn-outline-dark col-2 d-block p-2"> <h3 class="my-0 py-0" >XS</h3></button>
                         <button type="button" class="btn btn-outline-dark col-2 d-block p-2"><h3 class="my-0 py-0">S</h3></button>
                         <button type="button" class="btn btn-outline-dark col-2 d-block p-2"><h3 class="my-0 py-0">M</h3></button>
                         <button type="button" class="btn btn-outline-dark col-2 d-block p-2"><h3 class="my-0 py-0">L</h3></button>
                         <button type="button" class="btn btn-outline-dark col-2 d-block p-2 rounded-3"><h3 class="my-0 py-0">XL</h3></button>
+                        -->
+                        @foreach ($sizes as $s)
+                             <button type="button" class="btn btn-outline-dark col-2 d-block p-2 rounded-3"> <h3 class="my-0 py-0">{{$s->size}}</h3></button>
+                        @endforeach
                     </div>
                     <h1 id="price"class="mt-3">$000.00</h1>
                     <button type="button" class="btn btn-dark col-12 d-block py-3 rounded-3 mt-3"> <h4 class="my-0 py-0">Añadir al carrito</h4></button>
@@ -83,72 +80,63 @@
     <h3 class="fw-bold"> A otras personas también les interesó//Tus últimas visitas</h3>
     <div class="container-fluid" style="overflow-x:scroll;">
         <div class="row flex-row flex-nowrap">
-            <div class="col-3 d-flex justify-content-center">
+            
+            <div class="col-4 col-md-5 col-lg-3 d-flex justify-content-center ">
                 <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
+                    <div class="card" >
+                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>
+                        <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
+                        <h4 class="fw-bold card-text text-center" > $000.00 </h4>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-3 d-flex justify-content-center">
+            <div class="col-4 col-md-5 col-lg-3 d-flex justify-content-center ">
                 <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
+                    <div class="card" >
+                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>
+                        <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
+                        <h4 class="fw-bold card-text text-center" > $000.00 </h4>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-3 d-flex justify-content-center">
+            <div class="col-4 col-md-5 col-lg-3  d-flex justify-content-center ">
                 <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
+                    <div class="card" >
+                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>    
+                        <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
+                        <h4 class="fw-bold card-text text-center" > $000.00 </h4>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-3 d-flex justify-content-center">
+            <div class="col-4 col-md-5 col-lg-3 d-flex justify-content-center ">
                 <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
+                    <div class="card" >
+                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>
+                        <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
+                        <h4 class="fw-bold card-text text-center" > $000.00 </h4>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-3 d-flex justify-content-center">
+            <div class="col-4 col-md-5 col-lg-3 d-flex justify-content-center ">
                 <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
+                    <div class="card" >
+                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top rounded-0 img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>
+                        <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
+                        <h4 class="fw-bold card-text text-center" > $000.00 </h4>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-3 d-flex justify-content-center">
-                <a href="">
-                    <div class="card " style="width: 14rem;">
-                        <img src="{{ asset('assets/vestido.png') }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="fw-bold card-text text-center" >Vestido en manta</h5>
-                            <h4 class="fw-bold card-text text-center" > $000.00 </h4>    
-                        </div>
-                    </div>
-                </a>
-            </div>
+            
         </div>
     </div>
 </div>
