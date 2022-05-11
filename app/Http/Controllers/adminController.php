78280240af1b3314035119a2bf4956fcebb6b887
Zurisadaii <dianenisarahi22@gmail.com>
2022-05-11 12:50:01 -0500

    middleware y controlador admin

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\Product_category;
+use App\Models\Product_size;
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+        $this->middleware('checkadmin');
+    }
+
+    public function panelControl() {
+        return view('panel');
+    }
+
+    public function pantallaNP () {
+        return view('addProduct');
+    }
+}
