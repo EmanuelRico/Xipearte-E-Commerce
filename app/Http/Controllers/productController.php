e6d664813a1625c75b9f68589f79c6bb731580c5
Josue Daniel Vallejo Coss <jdanielvcoss@gmail.com>
2022-05-16 12:08:44 -0500

    controlador producto sin probar

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+        $this->middleware('checkadmin');
+    }
+
+    public function create(Request $request){
+        $product = New Product();
+        $product->name = $_POST['name'];
+        $product->description = $_POST['description'];
+        $product->price = (float)$_POST['price'];
+        $product->origin = $_POST['origin'];
+        $product->save();
+        foreach($request->file('images') as $file){
+            $imagen = new Image();
+            $name = $request->file('images')->getClientOriginalName();
+            $imagen->route = $request->file('images')->storeAs('img', $name,'public');
+            $imagen->product_id = $product->id;
+            $imagen->save();
+        }
+        $msg = "Creado exitosamente";
+        return view('dashboard',compact('msg'));
+    }
+
+    public function update(Request $request){
+        $product = Product::find((int)$request->id);
+        if($product){
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->price = (float)$request->price;
+            $product->origin = $request->origin;
+            $product->save();
+            foreach($request->file('images') as $file){
+                $imagen = new Image();
+                $name = $file->file('images')->getClientOriginalName();
+                $imagen->route = $file->file('images')->storeAs('img', $name,'public');
+                $imagen->product_id = $product->id;
+                $imagen->save();
+            }
+            $msg = "Actualizado exitosamente";
+            return view ('dashboard',compact('msg'));
+        }
+        else{
+            $msg = "No se pudo actualizar";
+            return view ('updateProduct',compact('msg'));
+        }
+    }
+}
