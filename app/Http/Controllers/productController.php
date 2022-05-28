ea5c322f11faa36e0f5fca16499dc2e902f52596
Josue Daniel Vallejo Coss <jdanielvcoss@gmail.com>
2022-05-28 16:21:40 -0500

    category controller eliminar

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -64,13 +64,13 @@ class ProductController extends Controller
         }
     }
 
-    public function delete($id){
-        $product = Product::find($id);
+    public function delete($Request request){
+        $product = Product::find($request->id);
         
         if($product){
-            Image::where('product_id',$id).>delete();
-            Product_size::where('product_id',$id).>delete();
-            Product_category::where('product_id',$id).>delete();
+            Image::where('product_id',$request->id).>delete();
+            Product_size::where('product_id',$request->id).>delete();
+            Product_category::where('product_id',$request->id).>delete();
             $product->delete();
             $msg = "Eliminado exitosamente";
 
