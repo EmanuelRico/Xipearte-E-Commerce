f570859d56b266cacf5b4d43081808109981f197
FernandoRodriguezG <fejerogo.17@gmail.com>
2022-09-18 17:53:41 -0500

    Stepper

--- resources/views/addDirection.blade.php
+++ resources/views/addDirection.blade.php
@@ -7,10 +7,19 @@
 
     <div class="container">
 
-        
             <div class="row justify-content-md-center ">
 
                 <div class="col col-lg-8">
+
+                    <div class="position-relative m-4">
+                        <div class="progress" style="height: 1px;">
+                          <div class="progress-bar" role="progressbar" aria-label="Progress" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <button type="button" class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">1</button>
+                        <button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">2</button>
+                        <button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-secondary rounded-pill" style="width: 2rem; height:2rem;">3</button>
+                      </div>
+
                     <h2 class="fw-bold pt-4 pb-4">Dirección del pedido</h2>
                     <form action="/saveAdd" method="POST" enctype="multipart/form-data" autocomplete="off">
                         @csrf 
--- resources/views/createOrder.blade.php
+++ resources/views/createOrder.blade.php
@@ -9,6 +9,16 @@
         <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
         <div class="row justify-content-md-center ">
             <div class="col col-lg-8">
+
+                <div class="position-relative m-4">
+                    <div class="progress" style="height: 1px;">
+                      <div class="progress-bar" role="progressbar" aria-label="Progress" style="width: 100%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <button type="button" class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">1</button>
+                    <button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">2</button>
+                    <button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">3</button>
+                  </div>
+
                 <h2 class="fw-bold pt-4 pb-4">Resumen del pedido</h2>
                 <h3 class="fw-bold pt-4 pb-4">Dirección</h3>
                 <table class="table table-condensed">
