b4925eccd40b959a8f162feb81660b257b8b7ec8
Emanuel Rico <emanuelrico8@gmail.com>
2022-11-06 14:33:51 -0600

    Bug fix multilples request

--- resources/views/addCategory.blade.php
+++ resources/views/addCategory.blade.php
@@ -7,7 +7,7 @@
 
 <div class="container">
 
-    <form action="/saveCategory" method="POST">
+    <form action="/saveCategory" method="POST" id="addCategory">
         @csrf
         <div class="row justify-content-md-center">
             <div class="col col-lg-8">
@@ -21,7 +21,7 @@
                     <textarea  id="categoryDescription"  required name="categoryDescription" class="form-control border-dark border-2" id="exampleFormControlTextarea1" rows="6"></textarea>
                 </div>
                 
-                <button type="submit" class="btn btn-dark col-12 d-block py-3 rounded-3 mt-3 mb-3"> 
+                <button type="submit" class="btn btn-dark col-12 d-block py-3 rounded-3 mt-3 mb-3" id="add_category_button"> 
                     Añadir categoría
                 </button>
             </div>
@@ -29,3 +29,38 @@
     </form>
 </div>
 @endsection
+
+@push('scripts')
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+<script>
+
+$(function(){
+    $("#addCategory").on("submit",function(e){
+        e.preventDefault();
+        var action = $(this).attr("action");
+        var method = $(this).attr("method");
+        var form_data = new FormData($(this)[0]);
+        var form = $(this);
+        $.ajax({
+            url:action,
+            dataType:'json',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: form_data,
+            type: method,
+            success: function(response) {
+                location.href = "/editarCategoria/"+response.id;
+            },
+            error: function(response){
+
+            },
+        });
+    $("#add_category_button").prop('disabled',true)
+        return false;
+    });
+});
+
+</script>
+@endpush
